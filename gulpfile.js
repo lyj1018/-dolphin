3cd4b7837c996bf5be9b7c6a4b5954914bbd5da3
lyj1018 <1942962158@qq.com>
2018-10-27 20:52:13 +0800

    haitun

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,46 @@
 //先导入插件
-let 
-	gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	cssnano = require('gulp-cssnano'),
-	rename = require('gulp-rename'),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat');
+ 
+	var gulp = require('gulp');
+	var sass = require('gulp-sass');
+	var cssnano = require('gulp-cssnano');
+	var rename = require('gulp-rename');
+	var uglify = require('gulp-uglify');
+	var concat = require('gulp-concat');
+	var imagemin =require('gulp-imagemin');
+	var watch = require('gulp-watch');
 	
 	
-//创建并发布任务
-gulp.task('dolp',function(){
-	
+// 创建并发布任务
+// gulp.task('dolp',function(){
+// 	console.log('hello');
+// })
+// 
+//编译sass
+gulp.task('sass',function(){
+	gulp.src('./src/sass/*.scss').pipe(sass()).pipe(cssnano()).pipe(gulp.dest('cssnano/css'));
 })
 
-//编译sass
-gulp.task('index',function(){
-	gulp.src('haituncun/images').pipe(uglify()).pipe(rename("suffix",".min"))
+gulp.task('images',function(){
+	 gulp.src('./src/images/*').pipe(imagemin()).pipe(gulp.dest('cssnano/img'));  
 })
+//监听
+gulp.task('watch',function(){
+	gulp.watch('./src/sass/*.scss',['sass']);
+	gulp.watch('./src/images/*',['images']);
+})
+
+
+// gulp.task('index',function(){
+// 	gulp.src('./src/images/*').pipe(rename({'suffix':'.min'})).pipe(imagemin()).pipe(gulp.dest('cssnano/img'));
+// })
+
+// gulp.task('watch',function(){
+// 	 gulp.watch('./src/sass/*.scss',['index']);
+// })
+
+// gulp.task('watch',function(){
+// 	 gulp.watch('./src/images/*',['index']);
+// })
+
+
+
